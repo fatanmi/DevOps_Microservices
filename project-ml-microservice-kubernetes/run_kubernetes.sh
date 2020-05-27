6d97205b2a92d06e951e0b8ecf96cf58272ce381
Ayobami <fatanmiayobami@gmail.com>
2020-05-27 06:08:31 +0000

    .

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -9,7 +9,7 @@
 dockerpath=haryorbami/myflaskapp
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl create deployment myflaskappnew --image=$dockerpath
+kubectl run myflaskapp --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
@@ -18,7 +18,11 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward deployment/myflaskappnew 8000:80
+kubectl port-forward myflaskapp 8000:80
 
 #kubectl expose pod myflaskappnew --port=80  --target-port=8000
+
+# To get logs
+kubectl logs `kubectl get pods -o=name`
+
 
